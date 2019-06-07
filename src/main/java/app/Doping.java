5f946dfbdad1093a926e165dcc94925e05fece77
byeongraeahn <xanbrx88@gmail.com>
2019-06-07 17:52:24 +0900

    수정

--- src/main/java/app/Doping.java
+++ src/main/java/app/Doping.java
@@ -7,18 +7,36 @@ import app.util.Common;
 
 public class Doping extends Common {
 
-    public static void main(String[] args) throws Exception {
+  public static void main(String[] args) throws Exception {
 
-      Robot robot = new Robot();
+    Runnable r = new secondDoping();
+    Thread thread = new Thread(r);
+    thread.start();
 
-        while (true) {
-          robot.delay(3000);
-          clickKey(KeyEvent.VK_7);
-          robot.delay(11000);
-          clickKey(KeyEvent.VK_9);
-          Thread.sleep(900000);
-        }
-      }
-    
+    Robot robot = new Robot();
+    while (true) {
+      robot.delay(3000);
+      clickKey(KeyEvent.VK_7);
+      // 15분
+      Thread.sleep(900000);
     }
-    
+  }
+}
+
+class secondDoping extends Common implements Runnable {
+
+  @Override
+  public void run() {
+
+    try {
+      Robot robot = new Robot();
+      while (true) {
+        robot.delay(5000);
+        clickKey(KeyEvent.VK_9);
+        // 20분
+        Thread.sleep(1200000);
+      }
+    } catch (Exception e) {
+    }
+  }
+}
